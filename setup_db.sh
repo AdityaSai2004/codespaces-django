ab38d680e8b1fef86c44c25ae324863cb0000c42
Aditya Sai SD <2004sai@gmail.com>
2023-12-10 19:40:36 +0000

    docker

--- setup_db.sh
+++ setup_db.sh
@@ -12,7 +12,7 @@ sleep 15
 # CREATE DATABASE mydatabase;
 # CREATE USER 'myuser'@'localhost' IDENTIFIED BY 'password';
 # GRANT ALL PRIVILEGES ON *.* TO 'myuser'@'localhost';
-mysql -e "CREATE DATABASE IF NOT EXISTS your_database_name;"
-mysql -e "CREATE USER 'your_database_user'@'%' IDENTIFIED BY 'your_database_password';"
-mysql -e "GRANT ALL PRIVILEGES ON your_database_name.* TO 'your_database_user'@'%';"
+mysql -e "CREATE DATABASE IF NOT EXISTS db;"
+mysql -e "CREATE USER 'user'@'%' IDENTIFIED BY 'user';"
+mysql -e "GRANT ALL PRIVILEGES ON db.* TO 'user'@'%';"
 echo "Database 'your_database_name' created with user 'your_database_user' and password 'your_database_password'."
